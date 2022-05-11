bf7ba5e0120c98a13b287b2aaa0203dac9e858c4
Mugilstracker <104926835+Mugilstracker@users.noreply.github.com>
2022-05-11 06:11:00 -0700

    Add files via upload

---
+++ AccessModifier.java
@@ -0,0 +1,25 @@
+package week1.day4;
+
+public class AccessModifier {
+	private void amsath() {
+		System.out.println("Biriyani");
+
+	}
+	public void Jeys() {
+		System.out.println("The 100");
+	}
+	void madhu() {
+		System.out.println("Bhaiya");
+	}
+	protected void dinesh() {
+		System.out.println("R15");
+
+	}
+public static void main(String[] args) {
+	AccessModifier am = new AccessModifier();
+	am.amsath();
+	am.madhu();
+	am.Jeys();
+}
+
+}
